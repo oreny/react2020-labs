c8194997dc170f5a47b4572dcef191fdf8722d3f
oreny <yardeni.oren@gmail.com>
2020-10-20 08:58:39 +0300

    question 3

---
+++ src/guess-game.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useState } from "react";
+
+export default function TimerInput(props) {
+    const [timeSec, setTimeSec] = useState(0);
+    const onInputChange = (e) => setTimeSec(Number(e.target.value) * e.target.name);
+
+    return (
+      <div>
+          { [1, 60, 3600].map( x => <div><input value={Math.floor((timeSec * 1000 / x))/1000} name={x} onChange={onInputChange} /></div>) }
+      </div>
+    );
+}
+
+
